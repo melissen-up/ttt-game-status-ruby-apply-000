9c02b660dc2e97beec7298a208c8d3342bf80f79
Melissa F <melissa.faulner@gmail.com>
2020-10-08 01:43:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,8 +24,8 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
 end
 
 def full?(board)
-  if board.all? do |token|
-    token == "X" || "O"
+  board.all? do |token|
+    if token == "X" || "O"
     return TRUE
   elsif
     FALSE 
