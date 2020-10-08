4cf5d456a9ff6ccc5e6a953301aeee4844593f1d
Melissa F <melissa.faulner@gmail.com>
2020-10-08 00:59:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,6 +16,10 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
       
-      position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return win_combo
+      else
+        return FALSE
+      end
     end
 end
