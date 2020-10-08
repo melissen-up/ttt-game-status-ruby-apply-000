4efe41fcd92df696ecaa503f8d0fa7eddc027097
Melissa F <melissa.faulner@gmail.com>
2020-10-08 00:33:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,12 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-  [0,1,2], # top row
-  [3,4,5], # middle row
-  [6,7,8], # bottom row
-  [0,3,6], # left column
-  [1,4,7], # middle column
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7], # middle column
   [2,5,8], # right column
   [0,4,8], # left diagonal
   [2,4,6] # right diagonal
