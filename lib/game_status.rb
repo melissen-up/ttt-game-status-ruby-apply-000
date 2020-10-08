a55f575a5e0830a5f224a713247b4dc1d2142db7
Melissa F <melissa.faulner@gmail.com>
2020-10-08 00:45:41 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,6 +10,8 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
     WIN_COMBINATIONS.each do |win_combo|
       if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
         return win_combo
+      elsif board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O"
+        return win_combo
       else 
         return nil  
       end
