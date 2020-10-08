62dd6d339c80ff86405cf3d70c5e8d3d2daf68a6
Melissa F <melissa.faulner@gmail.com>
2020-10-08 01:32:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,10 +24,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
 end
 
 def full?(board)
-  full_board = board.all? do |i|
+  board.all? do |i|
   i == "X" || "O"
-  end
-  incomplete_board = board.all? do |i|
-    !(i == "X" || "O")
   end
 end
