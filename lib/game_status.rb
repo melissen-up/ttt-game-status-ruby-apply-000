6c76598f7cfbd85fc597326a9ab700a5be38667b
Melissa F <melissa.faulner@gmail.com>
2020-10-08 01:01:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,9 +8,9 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
   
  def won?(board)
    WIN_COMBINATIONS.each do |win_combo|
-      win_index_1 = win_combo[0]
-      win_index_2 = win_combo[1]
-      win_index_3 = win_combo[2]
+      win_index_1 = win_combo[]
+      win_index_2 = win_combo[]
+      win_index_3 = win_combo[]
       
       position_1 = board[win_index_1]
       position_2 = board[win_index_2]
