7a5f1be9f060d490e4cb7d4a0a777017d643c6be
Melissa F <melissa.faulner@gmail.com>
2020-10-08 01:05:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,8 +18,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
       
       if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
         return win_combo
-      else
-        return FALSE
       end
     end
+    return FALSE 
 end
