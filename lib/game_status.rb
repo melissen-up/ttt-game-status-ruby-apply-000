0808df5ab4dbcb6ff5475b7156f13e9f6d1dd963
Melissa F <melissa.faulner@gmail.com>
2020-10-08 00:43:02 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,17 +8,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
   
  def won?(board)
     WIN_COMBINATIONS.each do |win_combo|
-      win_index_1 = win_combo[0]
-      win_index_2 = win_combo[1]
-      win_index_3 = win_combo[2]
-      
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
-      
-      if board[win_combo[0]] == "X" && position_2 == "X" && position_3 == "X"
-        return win_combo
-      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
         return win_combo
       else 
         return nil  
